144fdce622f4422ddbb309e9ddea0b4794ad2b63
littlepotato123 <sopanda0910@gmail.com>
2020-11-12 09:54:56 -0500

    Exploration 8 - Setup

---
+++ Exploration 8/Code/answer.py
@@ -0,0 +1,34 @@
+class Vehicle:
+    def __init__(self, name, milage):
+        self.name = name
+        self.milage = milage
+    
+    def drive(self, miles):
+        self.milage -= miles
+    
+    def __str__(self):
+        return f'{self.name} has {self.milage}'
+
+class Truck(Vehicle):
+    def __init__(self, milage, money):
+        super().__init__("Truck", milage)
+        self.money = money
+    
+    def buy(self, price):
+        self.money -= price 
+    
+    def sell(self, price):
+        self.money += price
+    
+    def __str__(self):
+        return f'{super().__str__()} and you have {self.money} left'
+    
+# Testing
+truck = Truck(300, 500)
+print(truck)
+truck.drive(20)
+print(truck)
+truck.sell(200)
+print(truck)
+truck.buy(300)
+print(truck)
